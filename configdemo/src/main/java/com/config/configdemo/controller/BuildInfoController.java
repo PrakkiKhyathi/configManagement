2ecfeda6ed470245f141e80e81a70a4f74ee41d2
PrakkiKhyathi <khyathiprakki@gmail.com>
2025-08-24 19:34:12 +0530

    configManagement

---
+++ configdemo/src/main/java/com/config/configdemo/controller/BuildInfoController.java
@@ -0,0 +1,31 @@
+package com.config.configdemo.controller;
+
+import com.config.configdemo.BuildInfo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RefreshScope
+public class BuildInfoController {
+    @Value("${build.id:default}")
+    private String buildId;
+    @Value("${build.version:default}")
+    private String buildVersion;
+    @Value("${build.name:default}")
+    private String buildName;
+//    private final BuildInfo buildInfo;
+//    @GetMapping("/build/info")
+//    public String getBuildInfo()
+//    {
+//        return "Build ID: "+ buildInfo.getId() +", version: "+ buildInfo.getVersion()+", Name: "+ buildInfo.getName();
+//    }
+        @GetMapping("/build/info")
+        public String getBuildInfo()
+        {
+            return "Build ID: "+ buildId +", version: "+ buildVersion+", Name: "+ buildName;
+        }
+}
