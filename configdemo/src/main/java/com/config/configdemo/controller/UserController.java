09fa8af423f21cbe9bd7a01ca61d8da12573a53c
PrakkiKhyathi <khyathiprakki@gmail.com>
2026-03-22 18:33:07 +0530

    UserController

---
+++ configdemo/src/main/java/com/config/configdemo/controller/UserController.java
@@ -0,0 +1,10 @@
+package com.config.configdemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class UserController {
+    @GetMapping("/user")
+    public String getUser() {
+        return "User Data";
+    }
+}
