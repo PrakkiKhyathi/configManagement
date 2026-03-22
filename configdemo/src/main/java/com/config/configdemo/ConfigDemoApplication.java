21bfb43a1decad7bbac889a85c87a820513443ce
PrakkiKhyathi <khyathiprakki@gmail.com>
2026-03-22 17:44:13 +0530

    partitial Updates

--- configdemo/src/main/java/com/config/configdemo/ConfigDemoApplication.java
+++ configdemo/src/main/java/com/config/configdemo/ConfigDemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ConfigDemoApplication {
 
 	public static void main(String[] args) {
-
+		System.out.print("Prakki Khyathi");
 
 		SpringApplication.run(ConfigDemoApplication.class, args);
 	}
